35358e967aadb95e0e5739b82f07087b798214fd
NMishchenko <mishchenko.my.s@nmu.one>
2021-11-08 16:15:35 +0200

    Task realisation in TK_3.py has been added

---
+++ TK_3.py
@@ -0,0 +1,7 @@
+def get_list_divided_by_avg(received_list):
+    result_list = []
+    list_avg = sum(received_list)/len(received_list)
+    for element in received_list:
+        new_element = element/list_avg
+        result_list.append(new_element)
+    return result_list
