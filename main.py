740f8f66924dab7743ad340232e831add73ff6e1
NMishchenko <mishchenko.my.s@nmu.one>
2021-11-08 17:13:25 +0200

    All errors with import have been fixed

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from TK-1 import get_list_from_console_input
+import importlib
+
 from TK_2 import get_min_max_from_list
 from TK_3 import get_list_divided_by_avg
 from TK_4 import get_list_multiplied_by_avg
-from TK-5 import get_list_sqrt
+
+TK_1 = importlib.import_module('TK-1')
+TK_5 = importlib.import_module('TK-5')
 
 if __name__ == '__main__':
-    print('Branch TK-1: ' + get_list_from_console_input(10))
-    print('Branch TK-2: ' + get_min_max_from_list([1,2,3,4,5]))
-    print('Branch TK-3: ' + get_list_divided_by_avg([1,2,3,4,5]))
-    print('Branch TK-4: ' + get_list_multiplied_by_avg([1,2,3,4,5]))
-    print('Branch TK-5: ' + get_list_sqrt([4,9,16,25,36]))
+    print('Branch TK-1: ' + str(TK_1.get_list_from_console_input(10)))
+    print('Branch TK-2: ' + str(get_min_max_from_list([1,2,3,4,5])))
+    print('Branch TK-3: ' + str(get_list_divided_by_avg([1,2,3,4,5])))
+    print('Branch TK-4: ' + str(get_list_multiplied_by_avg([1,2,3,4,5])))
+    print('Branch TK-5: ' + str(TK_5.get_list_sqrt([4,9,16,25,36])))
