180c3a87eaa56ee1dc073c1bbdb06e290999cdc8
NMishchenko <mishchenko.my.s@nmu.one>
2021-11-08 16:34:57 +0200

    Test cases in main.py has been added

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from TK-1 import get_list_from_console_input
+from TK_2 import get_min_max_from_list
+from TK_3 import get_list_divided_by_avg
+from TK-4 import get_list_multiplied_by_avg
+from TK-5 import get_list_sqrt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print('Branch TK-1: ' + get_list_from_console_input(10))
+    print('Branch TK-2: ' + get_min_max_from_list([1,2,3,4,5]))
+    print('Branch TK-3: ' + get_list_divided_by_avg([1,2,3,4,5]))
+    print('Branch TK-4: ' + get_list_multiplied_by_avg([1,2,3,4,5]))
+    print('Branch TK-5: ' + get_list_sqrt([4,9,16,25,36]))
